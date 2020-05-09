254829e79f5b12832ac89ee028df362a50271bdf
equusbambusoideae <zhu2qian1@gmail.com>
2020-05-09 16:46:02 +0900

    ちょっとだけ更新

--- WIP/dict_to_ztl.py
+++ WIP/dict_to_ztl.py
@@ -3,6 +3,18 @@
 
 # variables
 path = r".\\Python_public\\dict_to_ztl.ztl"
+src_path = r".\\Python_public\\src.eqsrc"
+
+
+class PyDictToZtl():
+    def __init__(self):
+        pass
+
+    def convert(self, src):
+        pass
+
+    def export(self, dest):
+        pass
 
 
 # define converter
@@ -10,10 +22,14 @@ def dict_to_ztl(src: dict, dest: str):
     import os
 
     if not os.path.exists(dest):  # if path does NOT exist
-        with open(dest, mode="w") as f:  # generate a file
+        with open(dest, mode="w") as f:  # create a file
             pass
 
-    return "converted"
+    # src_path を読み込んでパーザにかける
+    # 頻度が指定されていなければfreqに1を代入しておく
+    # ztl ファイルを出力
+
+    return None
 
 
 src = {
