3a737c4884f569d35e45bbe5f6c6508d1e7d7c5c
Amuzak, I <sennuma@gmail.com>
2020-06-13 00:58:44 +0900

    updated: language_generator.py

--- WIP/language_generator.py
+++ WIP/language_generator.py
@@ -9,58 +9,56 @@ def main():
         pass
 
     class Consonants:
-        pass
+        def __init__(self, characters: dict) -> object:
+            pass
 
     class Vowels:
         pass
 
-    class Consonant:
-        def __init__(
-            self, grapheme: str, voiced: bool, place: str, manner: str, **kwargs
-        ) -> object:
-            self.grapheme = str(grapheme)
-            self.voiced = voiced
-            self.place = place.title()
-            self.manner = manner.title()
-            try:
-                self.attributes = kwargs
-            except NameError:
-                self.attributes = None
+    # class Consonant:
+    #     def __init__(
+    #         self, grapheme: str, voiced: bool, place: str, manner: str, **kwargs
+    #     ) -> object:
+    #         self.grapheme = str(grapheme)
+    #         self.voiced = voiced
+    #         self.place = place.title()
+    #         self.manner = manner.title()
+    #         try:
+    #             self.attributes = kwargs
+    #         except NameError:
+    #             self.attributes = None
 
-    class Vowel:
-        def __init__(
-            self, grapheme: str, height: str, backness: str, roundness: bool, **kwargs
-        ) -> object:
-            self.grapheme = str(grapheme)
-            self.height = height.title()
-            self.backness = backness.title()
-            self.roundness = roundness
-            try:
-                self.attributes = kwargs
-            except NameError:
-                self.attributes = None
+    # class Vowel:
+    #     def __init__(
+    #         self, grapheme: str, height: str, backness: str, roundness: bool, **kwargs
+    #     ) -> object:
+    #         self.grapheme = str(grapheme)
+    #         self.height = height.title()
+    #         self.backness = backness.title()
+    #         self.roundness = roundness
+    #         try:
+    #             self.attributes = kwargs
+    #         except NameError:
+    #             self.attributes = None
 
     # a = Vowel("a", "open", "center", False, length="short")
     # i = Vowel("i", "close", "front", False, length="short")
     # u = Vowel("u", "close", "back", True, length="short")
 
     vowels = {
-        "a": ["open", "center", False, "short"],
-        "i": ["close", "front", False, "short"],
-        "u": ["close", "back", True, "short"],
+        "a": "open, central, unrounded, short",
+        "i": "close, front, undounded, short",
+        "u": "close, back, rounded, short"
     }
 
-    p = Consonant("p", False, "bilabial", "plosive")
-    t = Consonant("t", False, "dental", "plosive")
-    k = Consonant("k", False, "velar", "plosive")
-    h = Consonant("h", False, "glottal", "fricative")
-    w = Consonant("w", True, "bilabial", "approximant")
-    y = Consonant("y", True, "palatal", "approximant")
-    s = Consonant("s", False, "dental", "fricative")
-    r = Consonant("r", True, "dental", "trill")
-    l = Consonant("l", True, "dental", "lateral approximant")
-    m = Consonant("m", True, "bilabial", "nasal")
-    n = Consonant("n", True, "dental", "nasal")
+    consonants = {
+        "p": "voiceless, bilabial, plosive",
+        "t": "voiceless, dental, plosive",
+        "k": "voiceless, velar, plosive",
+        "h": "voiceless, glottal, fricative",
+        "w": "voiced, bilabial, approximant",
+        "y": "voiced, palatal, approximant"
+    }
 
 
 if __name__ == "__main__":
