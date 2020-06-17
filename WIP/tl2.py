9f66c654e744c5eb88a463d6085b3d2b1bfca3d1
equusbambusoideae <zhu2qian1@gmail.com>
2020-06-17 22:39:00 +0900

    Create tl2.py

---
+++ WIP/tl2.py
@@ -0,0 +1,21 @@
+import json
+from collections import OrderedDict
+import pprint
+
+consonants = {
+    "p": ["voiceless", "bilabial", "stop"],
+    "f": ["voiceless", "labiodental", "fricative"],
+    "t": ["voiceless", "dental", "plosive"],
+    "s": ["voiceless", "dental", "fricative"],
+    "h": ["voiceless", "glottal", "fricative"],
+    "w": ["voiced", "bilabial", "approximant"],
+    "m": ["voiced", "bilabial", "nasal"],
+    "n": ["voiced", "dental", "nasal"],
+    "j": ["voiced", "palatal", "approximant"],
+}
+
+vowels = {
+    "a": ["open", "central", "unrounded"],
+    "i": ["close", "front", "unrounded"],
+    "u": ["close", "back", "rounded"],
+}
