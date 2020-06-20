b4fa999aaabbaaf060f497269907ea61896935b3
Amuzak, I <sennuma@gmail.com>
2020-06-21 00:53:12 +0900

    Update t01.py
    pythonのsetはjsonに格納できないのでdictのvaluesをlist<->setに変換する関数を定義した

--- WIP/t01.py
+++ WIP/t01.py
@@ -1,18 +1,34 @@
 lang1_path = r".\\lang1\\lang1.json"
 
 
-def save(dest: str, dictionary: dict):
+def save_direct(dest: str, dictionary: dict):
     """facilitates dumping Python dict into json"""
     import json
     with open(dest, "w") as f:
         json.dump(dictionary, f, indent=4)
 
 
-def load(path: str) -> dict:
+def load_direct(path: str) -> dict:
     """facilitates loading json into Python dict"""
     import json
     with open(path, "r") as f:
         return json.load(f)
+
+
+def save_dictionary_with_set(dest: str, dictionary: dict):
+    pass
+
+
+def load_dictionary_values_as_set(path: str) -> dict:
+    pass
+
+
+def convert_values_to_set(d: dict):
+    try:
+        result = {k: set(d[k]) for k in d}
+    except TypeError as e:
+        raise e
+    return result
 
 
 def pick_conditioned(s: dict, c: set, n: int) -> str:
