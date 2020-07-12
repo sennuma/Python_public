15145939ffb07ca625aed7e5bfda68ac73744a5c
Amuzak, I <sennuma@gmail.com>
2020-07-13 00:33:58 +0900

    Create phonetic_data.py

---
+++ WIP/phonetic_data.py
@@ -0,0 +1,33 @@
+
+# consonant features
+bilabial = "bilabial"; labiodental = "labiodental"
+dental = "dental"
+alveolar = "alveolar"
+postalveolar = "postalveolar"
+retroflex = "retroflex"
+palatal = "palatal"
+velar = "velar"
+uvular = "uvular"
+glottal = "glottal"
+
+stop = plosive = "plosive"
+fricative = "fricative"
+affricate = "affricate"
+nasal = "nasal"
+approximant = "approximant"
+lateral_fricative = "lateral fricative"
+lateral_approximant ="letaral approximant"
+
+places = {bilabial, labiodental, dental, alveolar, postalveolar, retroflex, palatal, velar,uvular,glottal}
+manners = {plosive, fricative, affricate, nasal, approximant, lateral_fricative, lateral_approximant}
+
+# vowel features
+high = "high"
+mid = "mid"
+low = "low"
+front = "front"
+centre = center = "centre"
+back = "back"
+
+height = {high, mid, low}
+frontness = {front, centre, back}
