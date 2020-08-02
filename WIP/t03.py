78bb4a1795df563818caf6e7cf2a2f866e1bc0a7
Amuzak, I <sennuma@gmail.com>
2020-08-02 17:35:21 +0900

    Update t03.py

--- WIP/t03.py
+++ WIP/t03.py
@@ -1,14 +1,8 @@
-params = [
-    {
-        "coda obstrunt": False
-    },
-    {
-        "head initial": True
-    },
-    {
-        "null subject": True
-    },
-]
+params = dict(obstrunt_coda=True,
+              head_left=True,
+              null_subject=True,
+              null_onset=True,
+              NP_left=False)
 
 vowels = {
     "a": {"unrounded", "open", "back"},
@@ -18,6 +12,7 @@ vowels = {
     "o": {"rounded", "mid", "back"},
 }
 
+# %%
 consonants = {
     "k": {"voiceless", "velar", "plosive"},
     "g": {"voiced", "velar", "plosive"},
@@ -34,5 +29,17 @@ consonants = {
     "r": {"voiced", "alveolar", "tap"},
     "w": {"voiced", "bilabial", "approximant"},
 }
-
+# %%
 obstrunt = {"plosive", "fricative"}
+
+
+def create_set_from_dict(condition: set, sourcedict: dict) -> set:
+    assert type(sourcedict) == dict, "sourcedict should be a dictionary"
+    condition = set(condition)
+    return {k for k in sourcedict if condition <= sourcedict[k]}
+
+
+def generate_phonetic_array(vowels: dict, consonants: dict,
+                            params: dict) -> list:
+    assert type(vowels) == dict
+    assert type(consonants) == dict
