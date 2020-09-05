0485e78f9b9b0d01cd29575776c635b6d0d80897
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-06 01:14:11 +0900

    Update playground.py

--- WIP/playground.py
+++ WIP/playground.py
@@ -10,6 +10,31 @@ vowels = {
     "o": {"rounded", "mid", "back"},
 }
 
+heb_consonants = {
+    "’": {"voiceless", "glottal", "plosive"},
+    "b": {"voiced", "bilabial", "plosive"},
+    "g": {"voiced", "velar", "plosive"},
+    "d": {"voiced", "dental", "plosive"},
+    "h": {"voiceless", "glottal", "fricative"},
+    "w": {"voiced", "bilabial", "approximant"},
+    "z": {"voiced", "dental", "fricative"},
+    "ḥ": {"voiceless", "velar", "fricative"},
+    "ṭ": {"voiceless", "dental", "plosive"},
+    "y": {"voiced", "palatal", "approximant"},
+    "k": {"voiceless", "velar", "plosive"},
+    "l": {"voiced", "dental", "lateral approximant"},
+    "m": {"voiced", "nasal", "bilabial"},
+    "n": {"voiced", "dental", "nasal"},
+    "s": {"voiceless", "dental", "fricative"},
+    "‘": {"voiceless", "laryngael", "plosive"},
+    "p": {"voiceless", "bilabial", "plosive"},
+    "ṣ": {"voiceless", "dental", "affricate"},
+    "q": {"voiceless", "laryngael", "plosive"},
+    "r": {"voiced", "dental", "trill"},
+    "š": {"voiceless", "alveolar", "fricative"},
+    "t": {"voiced", "dental", "plosive"}
+}
+
 consonants = {
     "k": {"voiceless", "velar", "plosive"},
     "g": {"voiced", "velar", "plosive"},
@@ -40,11 +65,20 @@ params = {
     "vcluster_size": 2,
 }
 
+heb_params = {
+    "ccluster_size": 2,
+    "vcluster_size": 1,
+}
+
 phonemes = Phonemes(consonants, vowels)
 syllable = Syllable(phonemes, params)
 gentor = Phonetic_array_generator(syllable)
 
+heb_phonemes = Phonemes(heb_consonants, vowels)
+heb_syllable = Syllable(heb_phonemes, heb_params)
+heb_gentor = Phonetic_array_generator(heb_syllable)
+
 lst = []
 for i in range(128):
     lst.append(gentor.gen_naive_multiple())
-print(sorted(lst))
+print(lst)
