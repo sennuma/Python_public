d94b64fac2d4575d40f74adbb84bb2007e605882
Amuzak, I <sennuma@gmail.com>
2020-09-15 22:52:04 +0900

    Update 1_5_D.py

--- AOJ/ITP1/1_5_D.py
+++ AOJ/ITP1/1_5_D.py
@@ -1,3 +1,21 @@
-n = int(input())
-print(" ", end="")
-x = n
+def checknum(x: int, i: int) -> bool:
+    if x % 3 == 0:
+        print(f" {x}", end="")
+
+
+def include3(x: int, i: int) -> bool:
+    if "3" in str(x):
+        print(f" {x}", end="")
+        return True
+    return False
+
+
+def main(n: int) -> None:
+    for i in range(3, n + 1, 1):
+        if not include3(i, n):
+            checknum(i, n)
+    print()
+
+
+if __name__ == "__main__":
+    main(n=int(input()))
