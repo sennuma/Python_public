c3a86369cc416aa6877883cf87b42153bfe1fef0
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-20 01:09:33 +0900

    AOJ 16D消化

--- AOJ/ITP1/1_6/D.py
+++ AOJ/ITP1/1_6/D.py
@@ -1,1 +1,14 @@
-# src = https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_6_D
+# source = https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/6/ITP1_6_D
+
+n, m = map(int, input().split())
+a, b = [], []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+for i in range(m):
+    b.append(int(input()))
+
+for i in range(n):
+    c = 0
+    for j in range(m):
+        c += a[i][j] * b[j]
+    print(c)
---
+++ AOJ/ITP1/1_6/rd.py
@@ -0,0 +1,3 @@
+# source = https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/6/ITP1_6_D
+
+from numpy import 
