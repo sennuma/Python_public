0ffc7428f05a5ecad1a9415f169c570dc20509bc
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-21 09:49:41 +0900

    solved 17b

---
+++ AOJ/ITP1/1_7/B.py
@@ -0,0 +1,18 @@
+from itertools import combinations as comb
+
+data = []
+while 1:
+    n, x = map(int, input().split())
+    if n == x == 0:
+        break
+    data.append([n, x, 0])
+
+for d in data:
+    nums = list(range(1, d[0] + 1))
+    c = list(comb(nums, 3))
+    for l in c:
+        if sum(l) == d[1]:
+            d[2] += 1
+
+for d in data:
+    print(d[2])
