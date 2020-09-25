369df2ebedf529ac49701e52b73973cc2daec885
Amuzak, I <sennuma@gmail.com>
2020-09-25 21:28:19 +0900

    Update B.py

--- AOJ/ITP1/1_10/B.py
+++ AOJ/ITP1/1_10/B.py
@@ -1,1 +1,17 @@
 # src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/10/ITP1_10_B"
+
+from math import radians
+from math import sin
+from math import cos
+
+a, b, c = map(float, input().split())
+c = radians(c)
+
+s = 0.5 * a * b * sin(c)
+_ = a ** 2 + b ** 2 - 2 * a * b * cos(c)
+_ = _ ** 0.5
+l = a + b + _
+h = 2 * s / a
+
+for i in (s, l, h):
+    print("{:.6f}".format(i))
