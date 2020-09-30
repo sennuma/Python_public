b576946173bd5990203c7bdef932d92a028f2c51
Amuzak, I <sennuma@gmail.com>
2020-09-30 09:42:10 +0900

    Update b.py

--- AOJ/ITP1/1_11/b.py
+++ AOJ/ITP1/1_11/b.py
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
-# src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_11_B"
-run = 0
-=======
-src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_11_A"
-dbg = 1
->>>>>>> Stashed changes
+src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_11_B"
 
 
 class Die:
@@ -20,52 +14,23 @@ class Die:
 
     def __str__(self):
         return str(self.faces)
-<<<<<<< Updated upstream
 
-    def align(self, face, dest) -> None:
-        if (face, dest) in list(
-            zip(
-                ["front", "top", "back", "bottom"],
-                ["back", "bottom", "front", "top"],
-            )
-        ):
-            self.rotate("N")
-            self.rotate("N")
-        elif (face, dest) in list(zip(["right", "left"], ["left", "right"])):
+    def tofrri(self, to, fr):
+        ri = 0
+        for _ in range(2):
+            for _ in range(3):
+                for _ in range(4):
+                    if self.issametopfro(to, fr):
+                        ri = self.faces["right"]
+                    self.rotate("R")
+                self.rotate("N")
             self.rotate("R")
-            self.rotate("R")
-        elif (face, dest) in list(
-            zip(["right", "back", "left", "front"], ["back", "left", "front", "right"])
-        ):
-            self.rotate("R")
-        elif (face, dest) in list(
-            zip(["right", "front", "left", "back"], ["front", "left", "back", "right"])
-        ):
-            self.rotate("L")
-        elif (face, dest) in list(
-            zip(["top", "front", "bottom", "back"], ["front", "bottom", "back", "top"])
-        ):
             self.rotate("S")
-        elif (face, dest) in list(
-            zip(["top", "back", "bottom", "front"], ["back", "bottom", "front", "top"])
-        ):
-            self.rotate("N")
-        elif (face, dest) in list(
-            zip(["top", "right", "bottom", "left"], ["right", "top", "left", "bottom"])
-        ):
-            self.rotate("E")
-        elif (face, dest) in list(
-            zip(["top", "left", "bottom", "right"], ["left", "bottom", "right", "top"])
-        ):
-            self.rotate("W")
-=======
->>>>>>> Stashed changes
+        return ri
 
-    def mtd(self, t, f):
-        if t == self.faces["top"]:
-            pass
-        elif t == self.faces["front"]:
-            pass
+    def issametopfro(self, tp, fr):
+        if tp == self.faces["top"] and fr == self.faces["front"]:
+            return True
 
     def rotate(self, direction):
         if direction == "S":
@@ -104,7 +69,7 @@ class Die:
                 self.faces["right"],
                 self.faces["top"],
             )
-        else:  # direction == "W"
+        elif direction == "W":
             (
                 self.faces["top"],
                 self.faces["right"],
@@ -116,23 +81,31 @@ class Die:
                 self.faces["left"],
                 self.faces["top"],
             )
+        elif direction == "R":
+            (
+                self.faces["front"],
+                self.faces["right"],
+                self.faces["back"],
+                self.faces["left"],
+            ) = (
+                self.faces["right"],
+                self.faces["back"],
+                self.faces["left"],
+                self.faces["back"],
+            )
+        elif direction == "L":
+            (
+                self.faces["front"],
+                self.faces["left"],
+                self.faces["back"],
+                self.faces["right"],
+            ) = (
+                self.faces["left"],
+                self.faces["back"],
+                self.faces["right"],
+                self.faces["front"],
+            )
 
 
-<<<<<<< Updated upstream
-a = Die([1, 2, 3, 4, 5, 6])
-p = print
-
-if __name__ == "__main__" and run:
-    from copy import copy
-=======
-if __name__ == "__main__" and not dbg:
-    d = Die(input().split())
-    commands = input()
-    for command in commands:
-        d.rotate(command)
-    print(d.faces["top"])
->>>>>>> Stashed changes
-
-if __name__ == "__main__" and dbg:
-    d = Die([1, 2, 3, 4, 5, 6])
-    p = print
+# main
+d = Die([1, 2, 3, 4, 5, 6])
