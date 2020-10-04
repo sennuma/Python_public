bf5197382890991155bdae7338a4b75cc95cdbdb
Amuzak, I <sennuma@gmail.com>
2020-10-04 11:12:36 +0900

    AOJ

--- AOJ/ALDS1/2/b.py
+++ AOJ/ALDS1/2/b.py
@@ -1,30 +1,27 @@
 src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/2/ALDS1_2_B"
-__test = 1
+__test = 0
 
 # ---- solution ----
 
 
-def insertionsort(a):
-    n = len(a)
+def insertionsort(a) -> int:
     cnt = 0
-    for i in range(n):  # 整列部分のインデックス
+    n = len(a)
+    for i in range(n - 1):
         minj = i
-        for j in range(i, n):  # 非整列部分における最小値を探索
+        for j in range(i, n):
             if a[j] < a[minj]:
                 minj = j
-        a[i], a[minj] = a[minj], a[i]
-        cnt += 1
+        if i != minj:
+            a[i], a[minj] = a[minj], a[i]
+            cnt += 1
     return cnt
 
 
 # ---- process ----
 
 if __test:
-    a = [5, 6, 4, 2, 1, 3]
-    c = insertionsort(a)
-    print(*a)
-    print(c)
-    a = [5, 6, 4, 2, 1, 3]
+    pass
 else:
     input()
     a = list(map(int, input().split()))
---
+++ AOJ/ALDS1/2/c.py
@@ -0,0 +1,23 @@
+src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/2"
+__test = 1
+
+# ---- solution ----
+
+
+def bubblesort(c):
+    n = len(c)
+    for i in range(n - 1):
+        for j in range(n - 1, i, -1):
+            pass
+
+
+def selectionsort():
+    pass
+
+
+# ---- process ----
+
+if __test:
+    pass
+else:
+    pass
