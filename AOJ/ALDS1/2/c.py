f42beca7504f6fe3ad22c44258d68337d240038d
equusbambusoideae <zhu2qian1@gmail.com>
2020-10-07 12:26:59 +0900

    Update c.py

--- AOJ/ALDS1/2/c.py
+++ AOJ/ALDS1/2/c.py
@@ -1,28 +1,51 @@
-src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/2"
-__test = 1
+src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/2/ALDS1_2_C"
 
 # ---- solution ----
 
 
-def bubblesort(c):
+def bubblesort(c) -> None:
+    n = len(c)
+    flag = 1
+    while flag:
+        flag = 0
+        for j in range(n - 1, 0, -1):
+            if c[j][1] < c[j - 1][1]:
+                c[j], c[j - 1] = c[j - 1], c[j]
+                flag = 1
+
+
+def selectionsort(c) -> None:
     n = len(c)
     for i in range(n - 1):
-        for j in range(n - 1, i, -1):
-            pass
-
-
-def selectionsort():
-    pass
+        minj = i
+        for j in range(i, n - 1):
+            if c[j][1] < c[minj][1]:
+                minj = i
+        c[i], c[minj] = c[minj], c[i]
 
 
 # ---- process ----
-
+__test = 1
 if __test:
     cards = "H4 C9 S4 D2 C3".split()
-    ss = [cards[i][0] for i in range(len(cards))]
-    ns = [cards[i][1] for i in range(len(cards))]
+    c1 = cards[:]
+    c2 = cards[:]
+    bubblesort(c1)
+    print(*c1)
+    print("Stable")  # cuz stable
+
+    selectionsort(c2)
+    print(*c2)
+    print("Stable" if c1 == c2 else "Not stable")
 else:
     input()
     cards = input().split()
-    ss = [cards[i][0] for i in range(len(cards))]
-    ns = [cards[i][1] for i in range(len(cards))]
+    c1 = cards[:]
+    c2 = cards[:]
+    bubblesort(c1)
+    print(*c1)
+    print("Stable")  # cuz stable
+
+    selectionsort(c2)
+    print(*c2)
+    print("Stable" if c1 == c2 else "Not stable")
