ae060d54aaf352c3caa1d4a56d780129a9620725
Amuzak, I <sennuma@gmail.com>
2020-10-08 09:36:22 +0900

    d.oy

--- AOJ/ALDS1/2/d.py
+++ AOJ/ALDS1/2/d.py
@@ -1,8 +1,41 @@
-src = r""
+src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/2/ALDS1_2_D"
 
 # ---- solution ----
 
-pass
+
+def insertionsort(a, n, g):
+    cnt = 0
+    for i in range(g, n - 1):
+        v = a[i]
+        j = i - g
+        while j >= 0 and a[j] > v:
+            a[j + g] = a[j]
+            j = j - g
+            cnt += 1
+        a[j + g] = v
+
+
+def shellsort(a, n) -> int:
+    cnt = 0
+    m = 0
+    g = ["?", "?", "...", "?"]
+    for i in range(0, m - 1):
+        insertionsort(a, n, g[i])
+
+
+def t(a):
+    l = len(a)
+    h = 1
+    while h < l / 9:
+        h = h * 3 + 1
+    while h > 0:
+        for i in range(h, l):
+            j = i
+            while j >= h and a[j - h] > a[j]:
+                a[j], a[j - h] = a[j - h], a[j]
+                j -= h
+        h = int(h / 3)
+
 
 # ---- process ----
 
@@ -10,5 +43,12 @@ __test = 1
 if __test:
     pass
 else:
-    pass
-
+    n = int(input())
+    A = [5, 1, 4, 3, 2]
+    for i in range(n):
+        A.append(int(input()))
+    # print(int(m))
+    # print(*G)
+    # print(cnt)
+    # for i in range(n):
+    #     print(A[i])
