6d0cb61a9027589ee0cd8e9e1035698577d123ed
equusbambusoideae <zhu2qian1@gmail.com>
2020-10-14 10:29:51 +0900

    Update d.py

--- AOJ/ALDS1/3/d.py
+++ AOJ/ALDS1/3/d.py
@@ -1,15 +1,20 @@
 src = r"https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/3/ALDS1_3_D"
 
 # ---- solution ----
+land = input()
+hs, ls, area = [], [], 0
 
-pass
+for i in range(len(land)):
+    if land[i] == "\\":
+        hs.append(i)
+    elif land[i] == "/" and hs:
+        j = hs.pop()
+        a = i - j
+        area += a
 
-# ---- process ----
+        while ls and ls[-1][0] > j:
+            a += ls.pop()[1]
+        ls.append([j, a])
 
-
-def calcarea(m):
-    return 0
-
-
-# ---- test ----
-l = input()
+print(area)
+print(len(ls), *(a for j, a in ls))
