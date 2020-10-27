6e53d1bad593da0afa803ac73e1b63153a644d07
Amuzak, I <sennuma@gmail.com>
2020-10-28 01:37:33 +0900

    Update tutorial.py

--- API/tutorial/tutorial.py
+++ API/tutorial/tutorial.py
@@ -1,9 +1,6 @@
 import requests as req
 import random as rand
 
-# r = req.get("http://geoapi.heartrails.com/api/json?method=getAreas")
-# areas = r.json()["response"]["area"]
-
 r = req.get("http://geoapi.heartrails.com/api/json?method=getCities&prefecture=%E5%8C%97%E6%B5%B7%E9%81%93")
 cities = r.json()["response"]["location"]
 cities = tuple(i["city"] for i in cities)
