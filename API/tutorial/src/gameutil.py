f3c80d92a64765e1a3fed709da4a5626fcf73212
Amuzak, I <sennuma@gmail.com>
2020-10-30 02:32:07 +0900

    update

--- API/tutorial/src/gameutil.py
+++ API/tutorial/src/gameutil.py
@@ -1,11 +1,60 @@
-# from random import choice
+import src.geoapi as geoapi
 
 
-def closed_question(prompt: str) -> bool:
+def yn_question(prompt: str) -> bool:
     """"""
+    while 1:
+        ans: str = input(prompt)
+        if ans not in ("y", "n"):
+            print("y か n を入力してください．")
+            continue
+        elif ans == "y":
+            return True
+        return False
+
+
+def city_name_guesser(data: geoapi.GeoAPIInterface) -> None:
+    """
+    Start a console game where we guess the reading of a city, town, or village.
+    """
+    from random import choice
+
+    question: str
+    answer: str
+
+    print("{}の市町村名前当てゲームにようこそ！".format(data.prefecture))
+    print("終了するときは q を入力してください．\n")
+
+    while 1:
+        question, answer = choice(data.dataset)
+        response = input("「{}」の読みは何でしょうか？ひらがなで答えてください >>>".format(question))
+
+        if response == answer:
+            print("正解！")
+            data.dataset.remove(question)
+            print("残りは{}問あります".format(len(data.dataset)))
+        elif response == "q":
+            break
+        else:
+            print("不正解！正解は「{}」でした！".format(answer))
+    end_game()
+
+
+def end_game() -> None:
+    """
+    End the game showing credits and saying goodbye.
+    """
     pass
 
 
-def question_picker(questions: list) -> object:
-    """"""
-    pass
+def validate(arg: str) -> bool:
+    """
+    Check whether the prefecture is valid.
+    """
+    import json
+
+    with open(".\\json\\prefectures.json", encoding="utf8") as f:
+        prefectures = json.load(f)["prefectures"]
+    if arg not in prefectures:
+        return False
+    return True
--- API/tutorial/src/geoapi.py
+++ API/tutorial/src/geoapi.py
@@ -4,57 +4,92 @@ from urllib import parse
 import requests as req
 
 
-class Geoapi:
+class GeoAPIInterface:
     """
-    Fetching data via GeoAPI and its manipulation.
-
-    dataset: list[tuple] -> pass
+    Data of cities, towns, and villages in a prefecture.
+    Parameters
+    --------
+    prefecture: str
+        The name of the prefecture in kanji.
+    Attributes
+    --------
+    prefecture: str
+        The name of prefecture.
+    json: dict
+        Raw json.
+    contents: list
+        List of contents in the json.
+    dataset: list[tuple]
+        List[("name", "name_kana"), ...]
+        Optimized for games.
     """
 
     def __init__(self, prefecture):
         """
-        prefecute: str -> give it as kanji.
-
-        If the prefecture has ever been fetched, get the data from the local storage.
-        Otherwise, fetch the information via the API and dump it to local storage.
-
+        Fetch data of GeoAPI.
         """
+        self.prefecture = prefecture
         if hasfetched(prefecture):
-            with open(".\\json\\{}.json".format(prefecture), encoding="utf8") as f:
-                self.json = json.load(f)
-
+            self.json = fetch_pref_data(prefecture)
         else:
             self.response: req.Request = req.get(
                 "http://geoapi.heartrails.com/api/json?method=getCities&prefecture="
                 + parse.quote(prefecture)
             )
-            self.json: dict = self.response.json()
+            self.json = self.response.json()
 
-            with open(
-                ".\\json\\{}.json".format(prefecture), mode="w", encoding="utf8"
-            ) as f:
-                json.dump(self.json, f, indent=2, ensure_ascii=False)
-
-            with open(".\\json\\fetched.json", encoding="utf8") as f:
-                d: dict = json.load(f)
-                d["fetched"].append(prefecture)
-            with open(".\\json\\fetched.json", mode="w", encoding="utf8") as f:
-                json.dump(d, f, indent=2, ensure_ascii=False)
+            create_new_json(prefecture, self.json)
+            update_fetched_file(prefecture)
 
         self.contents: list = self.json["response"]["location"]
         self.dataset: list[tuple] = [(i["city"], i["city_kana"]) for i in self.contents]
 
 
-def fetch_pref_data(prefecture):
+def fetch_pref_data(prefecture: str) -> dict:
     """
-    fetch prefecture data with ease.
+    Fetch prefecture data.
+    Parameters
+    --------
+    prefecture: str
+        The name of prefecture in kanji.
+    Return
+    --------
+    d: dict
+        Raw json.
     """
     with open(".\\json\\{}.json".format(prefecture), encoding="utf8") as f:
-        d = json.load(f)
+        d: dict = json.load(f)
     return d
 
 
-def hasfetched(prefecture) -> bool:
+def update_fetched_file(prefecture: str) -> None:
+    """
+    Update fetched.json.
+    """
+    with open(".\\json\\fetched.json", encoding="utf8") as f:
+        d: dict = json.load(f)["fetched"]
+        d.append(prefecture)
+    with open(".\\json\\fetched.json", mode="w", encoding="utf8") as f:
+        json.dump(d, f, indent=2, ensure_ascii=False)
+    return None
+
+
+def create_new_json(prefecture: str, d: dict) -> None:
+    """
+    Create a new json file for a new prefecture.
+    Parameters
+    --------
+    prefecture: str
+        The name of the prefecture in kanji.
+    d: dict
+        The dict (json) of the prefecture.
+    """
+    with open(f".\\json\\{prefecture}.json", mode="w", encoding="utf8") as f:
+        json.dump(d, f, indent=2, ensure_ascii=False)
+    return None
+
+
+def hasfetched(prefecture: str) -> bool:
     """
     check if the prefecture has been fetched already.
     """
@@ -63,3 +98,16 @@ def hasfetched(prefecture) -> bool:
     if prefecture in d["fetched"]:
         return True
     return False
+
+
+def validate(arg: str) -> bool:
+    """
+    Check whether the prefecture is valid.
+    """
+    import json
+
+    with open(".\\json\\prefectures.json", encoding="utf8") as f:
+        prefectures = json.load(f)["prefectures"]
+    if arg not in prefectures:
+        return False
+    return True
