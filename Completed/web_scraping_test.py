132a595430bd0cf3bd5a92622051f2dac0293de8
Amuzak, I <zhu2qian1@gmail.com>
2021-02-12 09:36:47 +0900

    Create web_scraping_test.py

---
+++ Completed/web_scraping_test.py
@@ -0,0 +1,51 @@
+from typing import Iterable
+from bs4 import BeautifulSoup
+from requests import get
+from re import compile, Pattern
+
+
+def remtags(s: str, p: Pattern = compile(r"<.*?>")) -> str:
+    r = p.search(s)
+    if not r:
+        return s
+    else:
+        return remtags(s[: r.start()] + s[r.end() :], p)
+
+
+def remtags_in_list(l: list[str], p: Pattern = compile(r"<.*?>")) -> list:
+    r = []
+    for i in l:
+        r.append(remtags(i, p))
+    return r
+
+
+def soupfy(url: str) -> BeautifulSoup:
+    """return an instance of BeautifulSoap from the url given."""
+    return BeautifulSoup(get(url).content, "html.parser")
+
+
+def search_results(target_string: str, target_iterable: Iterable[str]) -> list:
+    """return a list of strings that contain target_string, from
+    target_iterable of str's."""
+    result = list()
+    p = compile(target_string)
+    for i in target_iterable:
+        if p.search(i):
+            result.append(i)
+    return result
+
+
+if __name__ == "__main__":
+    from pprint import pprint as PP
+
+    soup = soupfy("https://news.yahoo.co.jp/")
+
+    tags = soup.find_all(compile("^a"))
+    string_tags = [str(i) for i in tags]
+
+    r = search_results(r"/pickup/", string_tags)
+    print("\n" * 3)
+
+    abp = compile(r"<.*?>")
+    r = remtags_in_list(r)
+    PP(r)
